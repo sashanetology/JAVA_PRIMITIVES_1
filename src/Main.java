c014875daeffc368b9eec5399491590e8f9b653b
Саша <friend_adversary@mail.ru>
2023-03-05 19:57:16 +0300

    First correction

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,11 @@
 public class Main {
     public static void main(String[] args) {
 
-        int s = 333; // стоимость билета
-        int m = 20; // количество рублей для 1 бонусной мили
-        int b = s / m; // количество бонусных миль
+        int cost = 333; // стоимость билета
+        int precondition = 20; // количество рублей для 1 бонусной мили
+        int bonus = cost / precondition; // количество бонусных миль
 
-        System.out.println("Вам начислено " + b + " бонусных миль");
+        System.out.println("Вам начислено " + bonus + " бонусных миль");
 
     }
 }
